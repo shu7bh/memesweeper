95c72d6f28f7d9a51c4d76c1301789efbd152eff
shu7bh <shubh.agarwal@live.com>
2020-08-18 16:27:59 +0300

    Testing the SpriteCodex functions

--- Engine/Game.cpp
+++ Engine/Game.cpp
@@ -40,6 +40,22 @@ void Game::UpdateModel()
 {
 }
 
+#include "SpriteCodex.h"
 void Game::ComposeFrame()
 {
+
+	SpriteCodex::DrawTile0(Vec2(150, 250), gfx);
+	SpriteCodex::DrawTile1(Vec2(200, 250), gfx);
+	SpriteCodex::DrawTile2(Vec2(250, 250), gfx);
+	SpriteCodex::DrawTile3(Vec2(300, 250), gfx);
+	SpriteCodex::DrawTile4(Vec2(350, 250), gfx);
+	SpriteCodex::DrawTile5(Vec2(400, 250), gfx);
+	SpriteCodex::DrawTile6(Vec2(450, 250), gfx);
+	SpriteCodex::DrawTile7(Vec2(500, 250), gfx);
+	SpriteCodex::DrawTile8(Vec2(550, 250), gfx);
+	SpriteCodex::DrawTileButton(Vec2(600, 250), gfx);
+	SpriteCodex::DrawTileCross(Vec2(650, 250), gfx);
+	SpriteCodex::DrawTileFlag(Vec2(750, 250), gfx);
+	SpriteCodex::DrawTileBomb(Vec2(700, 150), gfx);
+	SpriteCodex::DrawTileBombRed(Vec2(650, 150), gfx);
 }
