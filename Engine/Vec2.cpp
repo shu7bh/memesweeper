3ac6e81e5ff2dd1dec8676a6d7c968d0aa65fa66
shu7bh <shubh.agarwal@live.com>
2020-08-19 21:08:13 +0300

    Overloaded operator + and += for Vec2 and int in class Vec2

--- Engine/Vec2.cpp
+++ Engine/Vec2.cpp
@@ -13,7 +13,17 @@ Vec2 Vec2::operator+( const Vec2& rhs ) const
 	return Vec2( x + rhs.x,y + rhs.y );
 }
 
+Vec2 Vec2::operator+(const int rhs) const
+{
+	return Vec2(x + rhs, y + rhs);
+}
+
 Vec2& Vec2::operator+=( const Vec2& rhs )
+{
+	return *this = *this + rhs;
+}
+
+Vec2& Vec2::operator+=(const int rhs)
 {
 	return *this = *this + rhs;
 }
--- Engine/Vec2.h
+++ Engine/Vec2.h
@@ -6,7 +6,9 @@ public:
 	Vec2() = default;
 	Vec2( int x_in,int y_in );
 	Vec2 operator+( const Vec2& rhs ) const;
+	Vec2 operator+(const int rhs) const;
 	Vec2& operator+=( const Vec2& rhs );
+	Vec2& operator+=(const int rhs);
 	Vec2 operator*( int rhs ) const;
 	Vec2 operator*(const Vec2 rhs) const;
 	Vec2& operator*=( int rhs );
