7520886e975a64f93e9522cba18425456a5b26ee
shu7bh <shubh.agarwal@live.com>
2020-08-20 13:04:47 +0300

    isContainedBy function created in Menu

--- Engine/Menu.cpp
+++ Engine/Menu.cpp
@@ -1,8 +1,25 @@
 #include "Menu.h"
 
+const std::array<RectI, 3> Menu::rects =
+{
+	RectI(RectI::FromCenter(Vec2(Graphics::ScreenWidth / 2, Graphics::ScreenHeight / 7),
+						SpriteCodex::sizeselWidth / 2, SpriteCodex::sizeselHeight / 2)),
+
+	RectI(RectI::FromCenter(Vec2(Graphics::ScreenWidth / 2, 3 * Graphics::ScreenHeight / 7),
+						SpriteCodex::sizeselWidth / 2, SpriteCodex::sizeselHeight / 2)),
+
+	RectI(RectI::FromCenter(Vec2(Graphics::ScreenWidth / 2, 5 * Graphics::ScreenHeight / 7),
+						SpriteCodex::sizeselWidth / 2, SpriteCodex::sizeselHeight / 2)),
+};
+
+bool Menu::isContainedBy(const Vec2& mousePos, const int i) const
+{
+	return rects[i].IsContainedBy( RectI::FromCenter(mousePos,0,0));
+}
+
 void Menu::draw(Graphics& gfx) const
 {
-	SpriteCodex::DrawSmall(Vec2(gfx.ScreenWidth / 2, gfx.ScreenHeight / 7), gfx);
-	SpriteCodex::DrawMedium(Vec2(gfx.ScreenWidth / 2, 3 * gfx.ScreenHeight / 7), gfx);
-	SpriteCodex::DrawLarge(Vec2(gfx.ScreenWidth / 2, 5 * gfx.ScreenHeight / 7), gfx);
+	SpriteCodex::DrawSmall(rects[0].GetCenter(), gfx);
+	SpriteCodex::DrawMedium(rects[1].GetCenter(), gfx);
+	SpriteCodex::DrawLarge(rects[2].GetCenter(), gfx);
 }
--- Engine/Menu.h
+++ Engine/Menu.h
@@ -1,10 +1,14 @@
 #pragma once
 #include "Graphics.h"
 #include "SpriteCodex.h"
+#include "RectI.h"
+#include <array>
 
 class Menu
 {
 private:
 public:
+	static const std::array<RectI, 3> rects;
+	bool isContainedBy( const Vec2& mousePos, const int i) const;
 	void draw(Graphics& gfx) const;
 };
